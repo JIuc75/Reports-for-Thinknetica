7558dd1613b123da5db71d5d6f5975d4d6db30b1
Евгений <jiuc75rus@gmail.com>
2019-04-04 20:18:07 +0900

    First commit

---
+++ Weight.rb
@@ -0,0 +1,9 @@
+puts "Здравствуйте, как вас зовут?"
+name = gets.to_s
+puts "Введите рост"
+growth = gets.to_i
+if growth <= 1
+  puts "Привет #{name}, твой оптимальный вес #{growth - 110}"
+else
+  puts "Привет #{name}, твой вес оптимальный"
+end
