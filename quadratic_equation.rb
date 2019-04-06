ad142becb7e5f1938a03d84c3a16b4ce65c956e3
Евгений <jiuc75rus@gmail.com>
2019-04-06 19:00:09 +0900

    Lesion_1_fixed1

---
+++ quadratic_equation.rb
@@ -0,0 +1,22 @@
+puts 'Введите коэффициент А'
+a = gets.to_f
+puts 'Введите коэффициент B'
+b = gets.to_f
+puts 'Введите коэффициент C'
+c = gets.to_f
+d = b**2 - 4 * a * c
+if d > 0
+  sqrt = Math.sqrt(D)
+  x1 = (-b + sqrt) / (2 * a)
+  x2 = (-b - sqrt) / (2 * a)
+  puts "Дискриминант= #{d}"
+  puts "Kорень x1= #{x1}"
+  puts "Корень x2= #{x2}"
+elsif d = 0
+  x1 = (-b) / (2 * a)
+  puts "Дискриминант= #{d}"
+  puts "Kорень x1,x2= #{x1}"
+else
+  puts "Дискриминант= #{d}"
+  puts 'Корней нет.'
+end
--- weight.rb
+++ weight.rb
@@ -1,9 +1,10 @@
 puts 'Здравствуйте, как вас зовут?'
-name = gets.to_s
+name = gets.chomp.capitalize
 puts 'Введите рост'
 growth = gets.to_i
-if growth <= 1
-  puts "Привет #{name}, твой оптимальный вес #{growth - 110}"
-else
+ideal_weight = growth - 110
+if ideal_weight < 0
   puts "Привет #{name}, твой вес оптимальный"
+else
+  puts "Привет #{name}, твой оптимальный вес #{ideal_weight}"
 end
