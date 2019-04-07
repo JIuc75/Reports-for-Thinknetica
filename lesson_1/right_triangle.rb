6044a6eec23a6a622dae7ecad8e0c8fc2db1b0a7
Евгений <jiuc75rus@gmail.com>
2019-04-07 16:40:50 +0900

    lesson_1_fixed2

--- lesson_1/right_triangle.rb
+++ lesson_1/right_triangle.rb
@@ -1,8 +1,8 @@
-puts 'Введите сторону'
+puts 'Введите сторону A'
 a = gets.to_f
-puts 'Введите сторону'
+puts 'Введите сторону B'
 b = gets.to_f
-puts 'Введите сторону'
+puts 'Введите сторону C'
 c = gets.to_f
 a1, b1, c1, = [a, b, c].sort!
 right_triangle = a1**2 + b1**2 == c1**2
