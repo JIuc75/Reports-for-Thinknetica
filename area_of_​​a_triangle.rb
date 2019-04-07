212c94c32cd311a756e0824a6af1ea4f72196b83
Евгений <jiuc75rus@gmail.com>
2019-04-07 16:36:56 +0900

    Lesion_1_fixed1

--- area_of_​​a_triangle.rb
+++
@@ -1,6 +0,0 @@
-puts 'Введите длинну основания треугольника'
-height = gets.to_f
-puts 'Введите высоту треугольника'
-base = gets.to_f
-area = 0.5 * (height * base)
-puts "лощадь треугольника = #{area}"
