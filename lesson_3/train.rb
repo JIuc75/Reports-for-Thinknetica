61f50703847435dbb3f425698398c08d57604cb8
Евгений <jiuc75rus@gmail.com>
2019-05-01 12:14:55 +0900

    lesson_3_fixed_4

--- lesson_3/train.rb
+++ lesson_3/train.rb
@@ -31,31 +31,27 @@ class Train
   end
 
   def forward
-    return if (@current_station == @route.end_station)
-    @current_station_index += 1
+    @current_station.accept_train(self)
     current_station
   end
 
   def backward
-    return if (@current_station == @route.start_station)
-    @current_station_index -= 1
+    @current_station.send_train(self) if @current_station
     current_station
   end
 
   def current_station
-    @current_station.send_train(self) if @current_station
     @current_station = @route.stations[@index_station]
-    @current_station.accept_train(self)
   end
 
   def next_station
-    return if (@current_station == @route.end_station)
-    @current_station += 1
+    return if next_station.nil?
+    @route.stations[@index_station]
   end
 
   def previous_station
-    return if (@current_station == @route.start_station)
-    @route.stations[@current_station_index - 1]
+    return if previous_station.nil?
+    @route.stations[@index_station.positive?]
   end
 
 end
