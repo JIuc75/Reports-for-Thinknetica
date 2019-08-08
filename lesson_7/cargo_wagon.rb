300cb61b40158040f603550ffae36a800b1fbb4d
evgeny <jiuc75rus@gmail.com>
2019-08-08 20:06:21 +0900

    lesson_7_fixed

--- lesson_7/cargo_wagon.rb
+++ lesson_7/cargo_wagon.rb
@@ -1,15 +1,9 @@
 class CargoWagon < Wagon
 
-  alias occupies_volume take_volume
-
-  def initialize(number, volume, occupies_volume)
+  def initialize(number, volume)
     super(number, 'cargo')
     @volume = volume
-    @occupies_volume = 0
-  end
-
-  def occupies_volume(volume)
-    take_volume(volume)
+    @occupies_volume = volume
   end
 
 end
--- lesson_7/passenger_wagon.rb
+++ lesson_7/passenger_wagon.rb
@@ -1,17 +1,17 @@
 class PassengerWagon < Wagon
 
-  alias take_the_place_of take_volume
-
   def initialize(number)
     super(number, 'passenger')
   end
 
-  def take_the_place_of
+  def take_the_place
     take_volume(1)
   end
 
   def take_volume(volume = 1)
     super(1)
   end
+
+  alias take_the_place take_volume
 
 end
